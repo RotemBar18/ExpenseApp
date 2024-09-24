0835f51d45a771831a82601d709ba29cb144fc6b
Rotem Bar <rotem68907@gmail.com>
2024-09-24 13:10:47 +0300

    database and backend on railway

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const dbConnection = mysql.createPool({
-    host: 'localhost',    
-    user: 'root',        
-    password: '', 
-    database: 'expensedb',
+    host: process.env.DB_HOST,          // Use the host from .env
+    user: process.env.DB_USER,          // Use the user from .env
+    password: process.env.DB_PASSWORD,  // Use the password from .env
+    database: process.env.DB_NAME,      // Use the database name from .env
+    port: process.env.DB_PORT,          // Use the port from .env
     waitForConnections: true,
     connectionLimit: 10, 
     queueLimit: 0
